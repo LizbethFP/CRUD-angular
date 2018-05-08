4d7b3f565858c4459fae6c5aafbf12e3d22722fc
LizbethFP <lizbeth.fp@gmail.com>
2018-05-07 19:59:20 -0500

    adds Class Product and CRUD methods

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,8 +1,41 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Product } from '../models/product'
 
 @Injectable()
 export class ProductService {
+  productList: AngularFireList<any>;
+  // escoger product específico, comienza en blanco y luego almacena momentáneamente el producto seleccionado
+  selectProduct: Product = new Product();
 
-  constructor() { }
+ 
+  constructor(private firebase: AngularFireDatabase) { }
 
+  getProdutc() {
+    return this.productList = this.firebase.list('products');
+  }
+
+  // product es un parámetro que será de tipo Clase Product
+  insertProduct(product: Product) {
+    this.productList.push({
+      name: product.name,
+      category: product.category,
+      location: product.location,
+      price: product.price
+    });
+  }
+
+  updateProduct(product: Product) {
+    // product.$key busca el producto según su key (es el primer parámetro), el sgt objeto es el segundo parámetro con los campos a actualizar
+    this.productList.update(product.$key, {
+      name: product.name,
+      category: product.category,
+      location: product.location,
+      price: product.price
+    });
+  }
+
+  deleteProduct($key:string) {
+    this.productList.remove($key);
+  }
 }
